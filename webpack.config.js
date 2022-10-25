5d4caf72954be8fc30e1eedf8b6e81a76cd7d59b
Duvan200207 <navud20@gmail.com>
2022-10-25 10:51:02 -0500

    Test 3

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+let tolo = /\.css$/;
+module.exports = {
+    mode: 'production',
+    entry: { 
+      index: '/src/app/index.js',
+      //shome: '/src/app/home.js',
+      voter: '/src/app/voter.js',
+      middleschool: '/src/app/middleschool.js',
+      highschool: '/src/app/highschool.js',
+    },
+    output: {
+      path: __dirname + '/src/public/js',
+      filename: '[name].bundle.js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        },
+      ] 
+    }
+  }
