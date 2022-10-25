51ac8ee5965ba9674578bba6eb5f24bef2ad9999
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2022-10-25 17:01:19 -0500

    pOSICIONAMIEO

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -8,15 +8,16 @@ export default function Card(){
 
     }, [])
     return(
-        <div className={styles["cards-container"]}>
-            <div className={styles["fixed"]}></div>
-            
-            <div className={`${styles["item"]} ${styles["image1"]}`} style={{backgroundImage: `url(./media/LAB01.jpg)`}}>
+        <section className="section-cards-container">
+            <div className={`${styles["item"]}`}></div>
+            <div className={styles["cards-container"]}>
+                <div className={`${styles["item"]} ${styles["image1"]}`} style={{backgroundImage: `url(./media/LAB01.jpg)`}}>
+                </div>
+                <div className={`${styles["item"]} ${styles["image2"]}`} style={{backgroundImage: `url(./media/LAB02.jpg)`}}>   
+                </div>
+                <div className={`${styles["item"]} ${styles["image3"]}`} style={{backgroundImage: `url(./media/LAB03.jpg)`}}> 
+                </div>
             </div>
-            <div className={`${styles["item"]} ${styles["image2"]}`} style={{backgroundImage: `url(./media/LAB02.jpg)`}}>   
-            </div>
-            <div className={`${styles["item"]} ${styles["image3"]}`} style={{backgroundImage: `url(./media/LAB03.jpg)`}}> 
-            </div>
-        </div>
+        </section>
     )
 }
