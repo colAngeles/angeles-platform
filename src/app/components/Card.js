d0465e5a90c10eb154cf42ec72679016c6987bbe
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2022-10-25 16:26:13 -0500

    pOSICIONAMIENTO

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -9,6 +9,8 @@ export default function Card(){
     }, [])
     return(
         <div className={styles["cards-container"]}>
+            <div className={styles["fixed"]}></div>
+            
             <div className={`${styles["item"]} ${styles["image1"]}`} style={{backgroundImage: `url(./media/LAB01.jpg)`}}>
             </div>
             <div className={`${styles["item"]} ${styles["image2"]}`} style={{backgroundImage: `url(./media/LAB02.jpg)`}}>   
