9b6b773480bf382f2eb0d3177b2ea0c898d583d1
Duvan200207 <navud20@gmail.com>
2022-10-29 11:38:57 -0500

    Main

---
+++ src/modules.py/sendmessage.py
@@ -0,0 +1,40 @@
+import yagmail
+import random, string
+
+def genToken():
+    a = random.choice(string.ascii_uppercase)
+    n = random.choice(string.ascii_letters)
+    g = random.choice(string.ascii_letters)
+    e = random.choice(string.ascii_letters)
+    l = random.choice(string.ascii_lowercase)
+    x = random.choice(string.ascii_letters)
+    s = random.choice(string.ascii_letters)
+    s = random.choice(string.digits)
+    return a + n + g + e + l + x + s
+
+yag = yagmail.SMTP(user="angelesmoodle@gmail.com", password="ygjpwmulsxtskkzi")
+def send(yag, toemail, name, token):
+        try:
+            yag.send(to=f"{toemail}", subject="Token de validación Colegio Los Ángeles", 
+                        contents=f'''
+                        ¡Cordial saludo señor(a) {name}!
+                        Queremos confirmarle los datos que le permitirán acceder a la plataforma de matriculas.
+                        
+                        <b>Token:</b> <b style="color: #f7901e;">{token}</b>
+                        
+                        <a href="http://45.65.233.35:1725/angeles/login/index.php">Aula Virtual Colegio Los Ángeles</a>
+                        
+                        Si tiene algún problema a la hora de acceder a la plataforma, puede escribir al correo duvanmotavita@colegiolosangelestunja.com o al número de <a style="color: #f7901e;" href="wa.me//+573228317980">WhatsApp</a>
+                        
+                        
+                        Duván Motavita Pérez
+                        Soporte Técnico, Colegio Los Ángeles.
+                        
+                        ''')
+            print(True)
+        except:
+            print(False)
+email = input()
+name = input()
+token = genToken()
+send(yag, email, name, token)
